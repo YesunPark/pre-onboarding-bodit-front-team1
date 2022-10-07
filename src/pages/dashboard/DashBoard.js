c1fb0a228c8e73ac954684bd0a35c6ee437a68cf
lkw1996 <lkw3703@gmail.com>
2022-10-07 18:53:43 +0900

    [Add] 데이터 fetch 이후 콘솔 출력 확인

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import Fetch from "./Fetch";
 
 const DashBoard = () => {
-	return <DashBoardContainer></DashBoardContainer>;
+  return (
+    <DashBoardContainer>
+      <Fetch />
+    </DashBoardContainer>
+  );
 };
 
 const DashBoardContainer = styled.div``;
