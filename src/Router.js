ddace4a0f738e138c5675ede6f5fbc2fa01eaf67
YesunPark <lynn08082@gmail.com>
2022-10-07 16:48:56 +0900

    [Add]: 센서리스트 기본파일 구조 생성

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
+import SensorList from './sensorlist/SensorList';
 
 function Router() {
 	return (
 		<BrowserRouter>
 			<GlobalStyles />
 			<Routes>
-				<Route path='/' />
+				<Route path='/' element={<SensorList />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/sensorlist/SensorList.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SensorList = () => {
+	return <SensorListContainer></SensorListContainer>;
+};
+
+const SensorListContainer = styled.div``;
+
+export default SensorList;
