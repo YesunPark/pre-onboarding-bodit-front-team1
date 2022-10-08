a872dd9299b99253c5dbf6304eac2f106d6fae9b
hrim <>
2022-10-09 02:53:34 +0900

    [Add]: 날짜형식

--- src/pages/dashboard/date/DatePick.js
+++ src/pages/dashboard/date/DatePick.js
@@ -2,11 +2,8 @@ import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import calendar from "../../../assets/images/calendar.png";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
 
-const DatePick = () => {
-  const [startDate, setStartDate] = useState(new Date());
-
+const DatePick = ({ startDate, setStartDate }) => {
   return (
     <>
       <DatePickContainer>
@@ -24,7 +21,6 @@ const DatePickContainer = styled.div`
   .react-datepicker__input-container {
     position: relative;
     width: auto;
-    //날짜 인풋
     input {
       padding: 7px 10px;
       border: none;
@@ -53,18 +49,15 @@ const DatePickContainer = styled.div`
     border: none;
     border-radius: 10px !important;
     overflow: hidden;
-    //컨테이너
   }
   .react-datepicker__header {
     background: #fff !important;
     border: none !important;
-    //달력 일 월부분
   }
   .react-datepicker__current-month {
     font-weight: 500;
   }
   .react-datepicker__navigation {
-    //월변경
     top: 30px !important;
     width: 20px !important;
     height: 20px !important;
@@ -77,28 +70,18 @@ const DatePickContainer = styled.div`
     right: 15px !important;
   }
   .react-datepicker__current-month {
-    //달
     padding: 30px 0 !important;
   }
   .react-datepicker__day-names {
-    //일 전체
     margin-bottom: 2px;
   }
   .react-datepicker__day-name {
-    //일 하나하나
     width: 1.9rem;
     font-weight: 500;
     line-height: 1.9rem;
     color: #666;
   }
-  .react-datepicker__month {
-    //1-31날짜부분
-  }
-  .react-datepicker__week {
-    //1-31 한줄 단위
-  }
   .react-datepicker__day {
-    //하루한칸
     width: 1.9rem;
     line-height: 1.9rem;
     :hover {
