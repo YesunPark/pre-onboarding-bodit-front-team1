38613d349f50b0105ba55fd1c3b20daefa5d7f19
lkw1996 <lkw3703@gmail.com>
2022-10-09 15:45:43 +0900

    [Design] Export 버튼 스타일링

--- src/pages/dashboard/export/ExportFile.js
+++ src/pages/dashboard/export/ExportFile.js
@@ -1,4 +1,5 @@
 import { CSVLink } from "react-csv";
+import styled from "styled-components";
 
 const ExportFile = ({ data }) => {
   const headers = [
@@ -9,10 +10,31 @@ const ExportFile = ({ data }) => {
   ];
 
   return (
-    <CSVLink data={data.feeds} filename="data" headers={headers}>
-      Download me
-    </CSVLink>
+    <ExportButtonContainer>
+      <CSVLink data={data.feeds} filename="data" headers={headers}>
+        Export
+      </CSVLink>
+    </ExportButtonContainer>
   );
 };
+
+const ExportButtonContainer = styled.div`
+  margin: 5% 2.5% 0 2.5%;
+  text-align: right;
+
+  a {
+    color: white;
+    display: inline-block;
+    text-decoration: none;
+    background-color: green;
+    padding: 4px 8px;
+    border-radius: 4px;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
 
 export default ExportFile;
